6c37f9def0b903da8d0b6b8873808a05882a7094
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-07-09 15:52:02 -0700

    Heroku!

--- scrape_mars.py
+++ scrape_mars.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 def init_browser():
-    return Browser("chrome", headless=True)
+    return Browser("chrome", executable_path=CHROMEDRIVER_PATH, headless=False)
 
 def scrape():
     url01 = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
